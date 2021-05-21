c69e8fc3e312095afcb767ff6f715b2d221b1bcd
j-o-e-d-o-e <d_joe@gmx.net>
2021-05-21 23:22:56 +0200

    fix ...

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -30,10 +30,23 @@ export class StatsComponent implements OnInit {
   private setData(stats: Stats) {
     this.stats = stats;
     // console.log(this.stats);
+    console.log(this.stats.plane_with_most_flights_within_one_day.day.date);
   }
 
   onDay(s: string) {
-    const date: string = s[0] + '-' + s[1] + '-' + s[2];
+    let day: string;
+    let month: string;
+    if (s[2].toString().length === 1) {
+      day = '0' + s[2];
+    } else {
+      day = s[2];
+    }
+    if (s[1].toString().length === 1) {
+      month = '0' + s[1];
+    } else {
+      month = s[1];
+    }
+    const date: string = s[0] + '-' + month + '-' + day;
     this.router.navigate(['/day', date]).catch();
   }
 
