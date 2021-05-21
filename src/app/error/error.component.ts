80f7130e2ae028fec8c831bcdb62e87762d0d192
j-o-e-d-o-e <d_joe@gmx.net>
2021-05-21 17:05:58 +0200

    back-Button added

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,16 +1,21 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+  styleUrls: ['./error.component.css', '../app.component.css']
 })
 export class ErrorComponent implements OnInit {
   @Input()
   message: string;
 
-  constructor() { }
+  constructor(private location: Location) { }
 
   ngOnInit() {
+  }
+
+  onBack() {
+    this.location.back();
   }
 }
