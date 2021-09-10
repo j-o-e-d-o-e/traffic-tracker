3c86757aa1dcd0b432e2b3cfff972ab38e1f5f39
joedoe <d_joe@gmx.net>
2021-09-10 22:18:51 +0200

    spinner centered

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
-  template: '<div class="lds-circle"><div></div></div>',
+  templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.css']
 })
 export class SpinnerComponent {
